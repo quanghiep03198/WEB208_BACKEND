ed23afc99c27a379022ce6d55bbc543a8ada477d
Quang Hiệp <quanghiep03198@gmail.com>
2023-03-03 02:34:38 +0700

    unit test with jest & supertest

--- src/configs/connectMongoDB.config.ts
+++ src/configs/connectMongoDB.config.ts
@@ -3,10 +3,15 @@ import mongoose, { MongooseError } from "mongoose";
 
 const connectMongoDB = async () => {
 	try {
-		const isProductionEnvironment = process.env.NODE_ENV?.toUpperCase()?.includes("PRODUCTION");
-		console.log(`[INFO] ${process.env.NODE_ENV?.trim().toUpperCase()} MODE`);
+		const isProductionEnvironment =
+			process.env.NODE_ENV?.toUpperCase()?.includes("PRODUCTION");
+		console.log(
+			`[INFO] ${process.env.NODE_ENV?.trim().toUpperCase()} MODE`
+		);
 		// -> check current environment to use remote database uri or local database uri
-		const dbUri = isProductionEnvironment ? process.env.REMOTE_DB_URI : process.env.LOCAL_DB_URI;
+		const dbUri = isProductionEnvironment
+			? process.env.REMOTE_DB_URI
+			: process.env.LOCAL_DB_URI;
 		mongoose.set("strictQuery", false);
 		const data = await mongoose.connect(dbUri as string, {
 			serverSelectionTimeoutMS: 5000, // cancel request after 5 seconds
