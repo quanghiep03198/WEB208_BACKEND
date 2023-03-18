c365c764bb63edb7d1de062bb71874b3963d1746
Quang Hiệp <quanghiep03198@gmail.com>
2023-03-18 20:10:12 +0700

    update webpack.config.js & redeploy

--- webpack.config.js
+++ webpack.config.js
@@ -2,48 +2,51 @@ const path = require("path");
 const TerserPlugin = require("terser-webpack-plugin");
 
 module.exports = {
-    mode: "production",
-    entry: "./src/server.ts",
-    output: {
-        path: path.resolve(__dirname, "dist"),
-        filename: "server.js",
-    },
-    target: "node",
-    resolve: {
-        extensions: [".ts", ".js"],
-    },
-    module: {
-        rules: [
-            {
-                test: /\.ts$/,
-                use: "ts-loader",
-                exclude: /node_modules/,
-            },
-        ],
-    },
-    optimization: {
-        minimize: true,
-        minimizer: [
-            new TerserPlugin({
-                terserOptions: {
-                    format: {
-                        comments: false,
-                    },
-                },
-                extractComments: false,
-            }),
-        ],
-    },
-    externals: {
-        bcrypt: "commonjs bcrypt",
-        compression: "commonjs compression",
-        cors: "commonjs cors",
-        dotenv: "commonjs dotenv",
-        express: "commonjs express",
-        "http-errors": "commonjs http-errors",
-        jsonwebtoken: "commonjs jsonwebtoken",
-        mongoose: "commonjs mongoose",
-        "mongoose-autopopulate": "commonjs mongoose-autopopulate",
-        morgan: "commonjs morgan",
-    },
+	mode: "production",
+	entry: "./src/server.ts",
+	output: {
+		path: path.resolve(__dirname, "dist"),
+		filename: "server.js",
+	},
+	target: "node",
+	resolve: {
+		extensions: [".ts", ".js"],
+	},
+	module: {
+		rules: [
+			{
+				test: /\.ts$/,
+				use: "ts-loader",
+				exclude: /node_modules/,
+			},
+		],
+	},
+	optimization: {
+		minimize: true,
+		minimizer: [
+			new TerserPlugin({
+				terserOptions: {
+					format: {
+						comments: false,
+					},
+				},
+				extractComments: false,
+			}),
+		],
+	},
+	externals: {
+		bcrypt: "commonjs bcrypt",
+		compression: "commonjs compression",
+		cors: "commonjs cors",
+		dotenv: "commonjs dotenv",
+		express: "commonjs express",
+		"http-errors": "commonjs http-errors",
+		jsonwebtoken: "commonjs jsonwebtoken",
+		swagger: "commonjs swagger",
+		"swagger-jsdoc": "commonjs swagger-jsdoc",
+		"swagger-ui-express": "commonjs swagger-ui-express",
+		mongoose: "commonjs mongoose",
+		"mongoose-autopopulate": "commonjs mongoose-autopopulate",
+		morgan: "commonjs morgan",
+	},
 };
